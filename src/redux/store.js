8150213eaf653f9bfc2a2288b71ebea776572369
apagason <109061082+apagason@users.noreply.github.com>
2022-10-12 13:55:08 +0800

    /

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./reducers/userReducer";
 import thunk from "redux-thunk";
 import promise from "redux-promise";
 import promiseMiddleware from "redux-promise-middleware";
 import logger from "redux-logger";
 
+// Reducers
+import userReducer from "./reducers/userReducer";
+import productReducer from "./reducers/productReducer";
+import blogReducer from "./reducers/blogReducer";
+
 export const store = configureStore({
   reducer: {
     activeUser: userReducer,
+    productList: productReducer,
+    blogList: blogReducer,
   },
-
   middleware: [thunk, promiseMiddleware, promise, logger],
 });
